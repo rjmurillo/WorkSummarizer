dec14e5bd4a6fc1d1d9a40ab39b7cdcad60aea8b
rimuri <rjmurillo@msn.com>
2014-02-10 17:11:53 -0800

    Addition of Graph API (usage following)

---
+++ src/Graph/GraphNode.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+
+namespace TeamFoundationServerWorkItemStateUpdater.Graph
+{
+    public class GraphNode<T> : Node<T>
+        where T : class
+    {
+        public GraphNode()
+            : base(default(T), new NodeListCollection<T>())
+        {
+            Costs = new List<int>();
+        }
+
+        public GraphNode(T value)
+            : base(value)
+        {
+            Costs = new List<int>();
+        }
+
+        public GraphNode(T value, NodeListCollection<T> neighbors)
+            : base(value, neighbors)
+        {
+            Costs = new List<int>();
+        }
+
+        public List<int> Costs { get; private set; }
+    }
+}
