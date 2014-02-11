92c500f493e02ca3d5554b8786fa1ad452dd29a0
Matt Westphal <tinyscuba@hotmail.com>
2014-02-11 11:49:57 -0800

    the interface

---
+++ src/TeamFoundationServerDataSource/ITfsData.cs
@@ -0,0 +1,30 @@
+﻿
+
+using System;
+using System.Collections.Generic;
+using Microsoft.TeamFoundation.VersionControl.Client;
+using Microsoft.TeamFoundation.WorkItemTracking.Client;
+
+namespace WorkSummarizer.TeamFoundationServerDataSource
+{
+    public interface ITfsData
+    {
+        /// <summary>
+        /// Gets a collection of workitems that changed within the time period
+        /// </summary>
+        /// <param name="tfsConnectionstring">the TFS TPC store to query</param>
+        /// <param name="startDate">start of the query time period</param>
+        /// <param name="endDate">end of the query time period</param>
+        /// <returns>collection of tfs workitems</returns>
+        IEnumerable<WorkItem> PullWorkItemsThatChanged(Uri tfsConnectionstring, DateTime startDate, DateTime endDate);
+
+        /// <summary>
+        /// Gets a collection of changesets that happened within the time period
+        /// </summary>
+        /// <param name="tfsConnectionstring">the TFS TPC store to query</param>
+        /// <param name="startDate">start of the query time period</param>
+        /// <param name="endDate">end of the query time period</param>
+        /// <returns>collection of tfs changesets</returns>
+        IEnumerable<Changeset> PullChangeSets(Uri tfsConnectionstring, DateTime startDate, DateTime endDate);
+    }
+}
