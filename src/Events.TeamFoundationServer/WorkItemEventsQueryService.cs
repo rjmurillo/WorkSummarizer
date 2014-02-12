f9079a8e77a482d12e00aecd7c8f7b71fe1abb5b
rimuri <rjmurillo@msn.com>
2014-02-12 11:43:59 -0800

    Added work item event translation (creation and revision)

--- src/Events.TeamFoundationServer/WorkItemEventsQueryService.cs
+++ src/Events.TeamFoundationServer/WorkItemEventsQueryService.cs
@@ -3,16 +3,51 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Common;
 using Events;
 using Microsoft.TeamFoundation;
+using Microsoft.TeamFoundation.WorkItemTracking.Client;
+using TeamFoundationServerWorkItemStateUpdater.Graph;
+using WorkSummarizer.TeamFoundationServerDataSource;
 
 namespace TfsCodeSwarm
 {
     public class WorkItemEventsQueryService : IEventQueryService
     {
+        public WorkItemEventsQueryService(Uri teamFoundationServerUri, string projectName)
+        {
+            TeamFoundationServer = teamFoundationServerUri;
+            Project = projectName;
+        }
+
+        public Uri TeamFoundationServer { get; private set; }
+        public string Project { get; private set; }
+
         public IEnumerable<Event> PullEvents(DateTime startDateTime, DateTime endDateTime)
         {
-            throw new NotImplementedException();
+            var source = new TfsData();
+            var wis = source.PullWorkItemsThatChanged(TeamFoundationServer, Project, startDateTime, endDateTime);
+
+            // Convert WI to Event
+            var retval = new List<Event>();
+            foreach (var wi in wis)
+            {
+                foreach (Revision r in wi.Revisions)
+                {
+                    var e = new Event();
+                    var p = new Participant() { Alias = (string)r.Fields["Changed by"].Value };
+
+                    e.Participants = new Graph<Participant> { p };
+                    e.Date = (DateTime)r.Fields["Changed date"].Value;
+                    e.Duration = 0d;
+                    e.Text = (string) r.Fields["History"].Value;
+                    e.EventType = r.Index == 0 ? "TFS Workitem Create" : "TFS Workitem Revision";
+                    e.Context = wi.Id;
+                    retval.Add(e);
+                }
+            }
+
+            return retval;
         }
     }
 }
