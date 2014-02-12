de9f36b5757769cca21ce27f66f6f08ba1fe9a3a
Josh Carrier <joshjcarrier@gmail.com>
2014-02-11 18:44:10 -0800

    Add Yammer event source (auth not fully functional)

---
+++ src/Events.Yammer/YammerEventsQueryService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using Yammer;
+
+namespace Events.Yammer
+{
+    public class YammerEventsQueryService
+    {
+        public static IEnumerable<YammerEvent> PullUserMessages(DateTime startFilterDate, DateTime endFilterDate)
+        {
+            YammerSession session = YammerAPI.LoginUserAsync("1GpcB6wgI9wqp8hB17PA", "R33cc1KWZ3uy6juE5DeNHWQKMjqzU3e7nvYkPLREeM", "https://www.yammer.com");
+            //var session = new YammerSession("e2c39b1509a85c85319ec8bdfaa247b0");
+            var currentUser = session.Users.GetCurrentUserAsync().Result;
+
+            return null;
+        }
+    }
+
+    public class YammerEvent
+    {
+    }
+}
--- src/WorkSummarizer/Program.cs
+++ src/WorkSummarizer/Program.cs
@@ -4,13 +4,21 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Events.Outlook;
+using Events.Yammer;
 
 namespace WorkSummarizer
 {
     class Program
     {
+        [STAThread] // this is for the Forms authentication dialog for Yammer auth....
         static void Main(string[] args)
         {
+            var outlookMeetings = OutlookQueryService.GetMeetings(DateTime.Parse("01/01/2013"), DateTime.Parse("02/11/2014"));
+            var foo = outlookMeetings.Select(x => x.Recipients.Count()).Average();
+
+            //var yammer = YammerEventsQueryService.PullUserMessages(DateTime.Parse("01/01/2013"),
+            //    DateTime.Parse("02/11/2014"));
+            
         }
     }
 }
