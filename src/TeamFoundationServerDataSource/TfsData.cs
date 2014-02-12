585dacaa0ab09ac36807d4a97f77e1daf1548893
Matt Westphal <tinyscuba@hotmail.com>
2014-02-11 16:04:11 -0800

    got rid of the partial class

--- src/TeamFoundationServerDataSource/TfsData.cs
+++ src/TeamFoundationServerDataSource/TfsData.cs
@@ -7,12 +7,37 @@ using System.Linq;
 using System.Net;
 using Microsoft.TeamFoundation;
 using Microsoft.TeamFoundation.Client;
+using Microsoft.TeamFoundation.VersionControl.Client;
 using Microsoft.TeamFoundation.WorkItemTracking.Client;
 
 namespace WorkSummarizer.TeamFoundationServerDataSource
 {
-    public partial class TfsData
+    public partial class TfsData : ITfsData
     {
+        public IEnumerable<Changeset> PullChangeSets(Uri tfsConnectionstring, string projectName, DateTime startDate, DateTime endDate)
+        {
+            try
+            {
+                VersionSpec versionFrom = new DateVersionSpec(startDate);
+                VersionSpec versionTo = new DateVersionSpec(endDate);
+
+                TfsTeamProjectCollection projectCollection =
+                    TfsTeamProjectCollectionFactory.GetTeamProjectCollection(tfsConnectionstring);
+                VersionControlServer versionControlServer = (VersionControlServer)projectCollection.GetService(typeof(VersionControlServer));
+                
+                IEnumerable changesetHistory =
+                    versionControlServer.QueryHistory("$/" + projectName + "/", VersionSpec.Latest, 0,
+                                                      RecursionType.Full, null, versionFrom, versionTo, int.MaxValue,
+                                                      false, false);
+
+                return changesetHistory.Cast<Changeset>().ToList();
+            }
+            catch (Exception ex)
+            {
+                throw new TeamFoundationException("Unable to get versionControlServer for TFS server " + tfsConnectionstring.AbsoluteUri, ex);
+            }
+        }
+        
         public IEnumerable<WorkItem> PullWorkItemsThatChanged(Uri tfsConnectionstring, string projectName, DateTime startDate, DateTime endDate)
         {
             WorkItemStore workItemStore = GetWorkItemStore(tfsConnectionstring);
