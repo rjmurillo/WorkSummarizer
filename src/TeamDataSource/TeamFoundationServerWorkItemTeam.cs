caa4efd7451855a5a929c277c47ae2fbc565bed6
rimuri <rjmurillo@msn.com>
2014-02-11 17:25:07 -0800

    Added filters for inferring participants from work items

--- src/TeamDataSource/TeamFoundationServerWorkItemTeam.cs
+++ src/TeamDataSource/TeamFoundationServerWorkItemTeam.cs
@@ -18,12 +18,37 @@ namespace TeamDataSource
 
         public IEnumerable<Participant> InferParticipantsFromWorkItems(IEnumerable<WorkItem> workItems)
         {
+            return InferParticipantsFromWorkItems(workItems, null);
+        }
+
+        public IEnumerable<Participant> InferParticipantsFromWorkItems(IEnumerable<WorkItem> workItems,
+            Func<WorkItem, bool> predicate)
+        {
+            return InferParticipantsFromWorkItems(workItems, predicate, null);
+        }
+
+        public IEnumerable<Participant> InferParticipantsFromWorkItems(IEnumerable<WorkItem> workItems,
+            Func<WorkItem, bool> workItemPredicate, Func<Revision, bool> workItemRevisionPredicate)
+        {
             List<Participant> participants = new List<Participant>();
+
+            if (workItemPredicate != null)
+            {
+                workItems = workItems.Where(workItemPredicate);
+            }
 
             foreach (WorkItem workItem in workItems)
             {
                 foreach (Revision revision in workItem.Revisions)
                 {
+                    if (workItemRevisionPredicate != null)
+                    {
+                        if (!workItemRevisionPredicate(revision))
+                        {
+                            continue;
+                        }
+                    }
+
                     participants.Add(new Participant() { Alias = (string)revision.Fields["Changed by"].Value });
                 }
             }
