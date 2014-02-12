1c83c7b2d4581bce81a3c40026268a369afb5a30
ben bamesberger <sortof@gmail.com>
2014-02-12 15:42:25 -0800

    x

--- src/WorkSummarizer/Program.cs
+++ src/WorkSummarizer/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using WorkSummarizer.TeamFoundationServerDataSource;
 
 namespace WorkSummarizer
 {
@@ -7,6 +8,13 @@ namespace WorkSummarizer
         [STAThread] // this is for the Forms authentication dialog for Yammer auth....
         static void Main(string[] args)
         {
+
+            var tfs = new TfsData();
+        //    var changesets = tfs.PullChangesets(new Uri("http://vstfcodebox:8080/tfs/Engineering_Excellence"), "EE_Engineering", DateTime.Parse("1/1/2014"), DateTime.Today);
+
+            var workItems = tfs.PullWorkItemsThatChanged(new Uri("http://vstfcodebox:8080/tfs/Engineering_Excellence"), "EE_Engineering", DateTime.Parse("1/22/2014"), DateTime.Today);
+
+            var graph = TfsHelper.BuildWorkItemGraph(workItems);
         }
     }
 }
