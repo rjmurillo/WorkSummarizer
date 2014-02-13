88ece2227c174ed409482c97d89fa93af42b69c4
Josh Carrier <joshjcarrier@gmail.com>
2014-02-13 12:30:55 -0800

    Scaffolding for remaining GUI input options

---
+++ src/WorkSummarizerGUI/ViewModels/EventSourceViewModel.cs
@@ -0,0 +1,18 @@
+﻿
+namespace WorkSummarizerGUI.ViewModels
+{
+    public class EventSourceViewModel : ViewModelBase
+    {
+        private readonly string m_name;
+
+        public EventSourceViewModel()
+        {
+            m_name = "Jocarrie";
+        }
+
+        public string Name
+        {
+            get { return m_name; }
+        }
+    }
+}
---
+++ src/WorkSummarizerGUI/ViewModels/ReportingSinkViewModel.cs
@@ -0,0 +1,18 @@
+﻿
+namespace WorkSummarizerGUI.ViewModels
+{
+    public class ReportingSinkViewModel : ViewModelBase
+    {
+        private readonly string m_name;
+
+        public ReportingSinkViewModel()
+        {
+            m_name = "Edkotows";
+        }
+
+        public string Name
+        {
+            get { return m_name; }
+        }
+    }
+}
