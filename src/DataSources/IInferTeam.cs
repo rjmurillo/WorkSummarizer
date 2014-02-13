e060917bc95dfa41ac1f06b103aec97216e96407
rimuri <rjmurillo@msn.com>
2014-02-13 11:42:37 -0800

    Added new interface to allow for team inference

---
+++ src/DataSources/IInferTeam.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using Common;
+
+namespace DataSources
+{
+    public interface IInferTeam<T>
+    {
+        IEnumerable<Participant> InferParticipants(IEnumerable<T> data);
+
+        IEnumerable<Participant> InferParticipants(IEnumerable<T> workItems, Func<T, bool> predicate);
+    }
+}
