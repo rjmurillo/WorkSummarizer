80206d7ffcf27b00c75f0dc4038b6dbfd78c1de7
Josh Carrier <joshjcarrier@gmail.com>
2014-02-13 14:19:01 -0800

    Add and use event source selection in GUI + downgrade Outlook event source to 4.5 for client compatibilty

--- src/WorkSummarizerGUI/ViewModels/EventSourceViewModel.cs
+++ src/WorkSummarizerGUI/ViewModels/EventSourceViewModel.cs
@@ -1,13 +1,36 @@
 ﻿
+using System;
+
 namespace WorkSummarizerGUI.ViewModels
 {
     public class EventSourceViewModel : ViewModelBase
     {
+        private readonly Type m_eventSourceType;
         private readonly string m_name;
+        private bool m_isSelected;
 
-        public EventSourceViewModel()
+        public EventSourceViewModel(string name, Type eventSourceType)
         {
-            m_name = "Jocarrie";
+            m_eventSourceType = eventSourceType;
+            m_name = name;
+        }
+
+        public Type EventSourceType
+        {
+            get { return m_eventSourceType; }
+        }
+
+        public bool IsSelected
+        {
+            get 
+            { 
+                return m_isSelected; 
+            }
+            set 
+            { 
+                m_isSelected = value;
+                OnPropertyChanged();
+            }
         }
 
         public string Name
--- src/WorkSummarizerGUI/ViewModels/MainViewModel.cs
+++ src/WorkSummarizerGUI/ViewModels/MainViewModel.cs
@@ -1,12 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
 using System.Linq;
-using System.Runtime.CompilerServices;
-using System.Text;
-using System.Threading.Tasks;
+using System.Windows.Threading;
+using Events;
+using Events.CodeFlow;
+using Events.Connect;
+using Events.Kudos;
+using Events.ManicTime;
+using Events.Outlook;
+using Events.TeamFoundationServer;
 using Events.Yammer;
-using Extensibility;
 using Renders;
 using Renders.Excel;
 using WorkSummarizer;
@@ -24,7 +27,17 @@ namespace WorkSummarizerGUI.ViewModels
 
         public MainViewModel()
         {
-            m_eventSources = new[] {new EventSourceViewModel(), new EventSourceViewModel()};
+            m_eventSources = new[] 
+            {
+                new EventSourceViewModel("Fake", typeof(FakeEventsPlugin)) { IsSelected = true }, 
+                new EventSourceViewModel("CodeFlow", typeof(CodeFlowPlugin)), 
+                new EventSourceViewModel("Connect", typeof(ConnectPlugin)), 
+                new EventSourceViewModel("Kudos", typeof(KudosPlugin)), 
+                new EventSourceViewModel("ManicTime", typeof(ManicTimePlugin)), 
+                new EventSourceViewModel("Outlook", typeof(OutlookPlugin)), 
+                new EventSourceViewModel("Team Foundation Server", typeof(TeamFoundationServerPlugin)), 
+                new EventSourceViewModel("Yammer", typeof(YammerPlugin)), 
+            };
             m_endLocalTime = DateTime.Now;
             m_reportingSinks = new[] { new ReportingSinkViewModel(), new ReportingSinkViewModel() };
             m_startLocalTime = DateTime.Now.AddMonths(-1);
@@ -72,43 +85,54 @@ namespace WorkSummarizerGUI.ViewModels
 
         public void Generate()
         {
-            var plugins = new List<Type>();
+            IsBusy = true;
 
-            //plugins.Add(typeof(FakeEventsPlugin));
-            // plugins.Add(typeof(CodeFlowPlugin));
-            // plugins.Add(typeof(ConnectPlugin));
-            // plugins.Add(typeof(KudosPlugin));
-            // plugins.Add(typeof(ManicTimePlugin));
-            // plugins.Add(typeof(OutlookPlugin));
-            // plugins.Add(typeof(TeamFoundationServerPlugin));
-            plugins.Add(typeof(YammerPlugin));
+            var selectedEventSourceTypes = EventSources.Where(p => p.IsSelected)
+                                                       .Select(p => p.EventSourceType)
+                                                       .ToList();
 
-            var pluginRuntime = new PluginRuntime();
-
-            var renders = new List<IRenderEvents>();
-            renders.Add(new ExcelWriteEvents());
-
-            pluginRuntime.Start(plugins);
-
-            foreach (var eventQueryServiceRegistration in pluginRuntime.EventQueryServices)
+            if (selectedEventSourceTypes.Any())
             {
-                Console.WriteLine("Querying from event query service: " + eventQueryServiceRegistration.Key);
-                var evts = eventQueryServiceRegistration.Value.PullEvents(new DateTime(2014, 1, 1), new DateTime(2014, 2, 14));
+                DispatcherFrame frame = new DispatcherFrame();
+                Dispatcher.CurrentDispatcher.BeginInvoke(
+                    DispatcherPriority.Background,
+                    new DispatcherOperationCallback(o =>
+                    {
+                        frame.Continue = false;
 
-                foreach (IRenderEvents render in renders)
-                {
-                    render.WriteOut(eventQueryServiceRegistration.Key, evts);
-                }
+                        var pluginRuntime = new PluginRuntime();
 
-                Console.WriteLine();
-            }
+                        var renders = new List<IRenderEvents>();
+                        renders.Add(new ExcelWriteEvents());
 
-            if (!pluginRuntime.EventQueryServices.Any())
-            {
-                Console.WriteLine("No event query services registered!");
+                        pluginRuntime.Start(selectedEventSourceTypes);
+
+                        foreach (var eventQueryServiceRegistration in pluginRuntime.EventQueryServices)
+                        {
+                            Console.WriteLine("Querying from event query service: " + eventQueryServiceRegistration.Key);
+                            var evts = eventQueryServiceRegistration.Value.PullEvents(new DateTime(2014, 1, 1),
+                                new DateTime(2014, 2, 14));
+
+                            foreach (IRenderEvents render in renders)
+                            {
+                                render.WriteOut(eventQueryServiceRegistration.Key, evts);
+                            }
+
+                            Console.WriteLine();
+                        }
+
+                        if (!pluginRuntime.EventQueryServices.Any())
+                        {
+                            Console.WriteLine("No event query services registered!");
+                        }
+
+                        return null;
+                    }), frame);
+                Dispatcher.PushFrame(frame);
             }
 
             Console.WriteLine("Done.");
+            IsBusy = false;
         }
     }
 }
