6c57231d4111d0c9e4e1185253bc9a8b867432a6
Edmond Kotowski <ekotowski@gmail.com>
2014-02-12 17:27:57 -0800

    Fixed some bugs in OutlookDataProvider

--- src/OutlookDataSource/OutlookDataProvider.cs
+++ src/OutlookDataSource/OutlookDataProvider.cs
@@ -16,35 +16,55 @@ namespace OutlookDataSource
 
     public class OutlookDataProvider : IOutlookDataProvider
     {
-        public IEnumerable<OutlookItem> GetMeetings(DateTime startDateUtc, DateTime endDateUtc)
-        {
-            var items = GetItems(startDateUtc, endDateUtc, Microsoft.Office.Interop.Outlook.OlDefaultFolders.olFolderCalendar, "[Start]");
-            return items.OfType<AppointmentItem>().Select(mail => new OutlookItem(mail.Subject, mail.Body, mail.CreationTime, mail.CreationTime, mail.Recipients.Cast<Recipient>().Select(x => x.Name))).ToList();
-        }
 
-        public IEnumerable<OutlookItem> GetEmails(DateTime startDateUtc, DateTime endDateUtc)
-        {
-            var items = GetItems(startDateUtc, endDateUtc, Microsoft.Office.Interop.Outlook.OlDefaultFolders.olFolderInbox, "[CreationTime]");
-            return items.OfType<MailItem>().Select(mail => new OutlookItem(mail.Subject, mail.Body, mail.CreationTime, mail.CreationTime, mail.Recipients.Cast<Recipient>().Select(x => x.Name))).ToList();
-        }
-
-        private Microsoft.Office.Interop.Outlook.Items GetItems(DateTime startDateUtc, DateTime endDateUtc, OlDefaultFolders defaultFolders, string filterDateFormat)
+        public IEnumerable<OutlookItem> GetMeetings(DateTime startFilterDate, DateTime endFilterDate)
         {
             Microsoft.Office.Interop.Outlook.Application oApp = new Microsoft.Office.Interop.Outlook.Application();
             var mapiNamespace = oApp.GetNamespace("MAPI");
 
-            var calendarFolder = mapiNamespace.GetDefaultFolder(defaultFolders);
+            var calendarFolder =
+                mapiNamespace.GetDefaultFolder(Microsoft.Office.Interop.Outlook.OlDefaultFolders.olFolderCalendar);
 
             var calendarItems = calendarFolder.Items;
 
-            calendarItems.Sort(filterDateFormat, false);
+            calendarItems.Sort("[Start]", false);
             calendarItems.IncludeRecurrences = true;
 
-            var filter = filterDateFormat + " >= \"" + startDateUtc.ToShortDateString() + "\" and " + filterDateFormat + " <=\"" + endDateUtc.ToShortDateString() + "\"";
+            var filter = "[Start] >= \"" + startFilterDate.ToShortDateString() + "\" and [Start] <=\"" +
+                         endFilterDate.ToShortDateString() + "\"";
+
+            var item = calendarItems.Find(filter);
+
+            var itemsList = new List<OutlookItem>();
+            while (item != null)
+            {
+                var foo = item as Microsoft.Office.Interop.Outlook.AppointmentItem;
+                if (foo == null)
+                {
+                    continue;
+                }
+
+                itemsList.Add(new OutlookItem(foo.Subject, foo.Body, foo.StartUTC, foo.EndUTC,
+                    foo.Recipients.Cast<Recipient>().Select(x => x.Name)));
+                item = calendarItems.FindNext();
+            }
+
+            return itemsList;
+        }
+
+        public IEnumerable<OutlookItem> GetEmails(DateTime startDateUtc, DateTime endDateUtc)
+        {
+            Microsoft.Office.Interop.Outlook.Application oApp = new Microsoft.Office.Interop.Outlook.Application();
+            var mapiNamespace = oApp.GetNamespace("MAPI");
+
+            var calendarFolder = mapiNamespace.GetDefaultFolder(Microsoft.Office.Interop.Outlook.OlDefaultFolders.olFolderInbox);
+
+            var filter = "[CreationTime] >= \"" + startDateUtc.ToShortDateString() + "\" and [CreationTime] <=\"" +
+             endDateUtc.ToShortDateString() + "\"";
 
             Microsoft.Office.Interop.Outlook.Items restrictedItems = calendarFolder.Items.Restrict(filter);
 
-            return restrictedItems;
+            return restrictedItems.OfType<MailItem>().Select(mail => new OutlookItem(mail.Subject, mail.Body, mail.CreationTime, mail.CreationTime, mail.Recipients.Cast<Recipient>().Select(x => x.Name))).ToList();
         }
     }
 }
