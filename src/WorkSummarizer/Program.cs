e10630c122ead7f9915d169f25b2af3d79d2b066
Matt Westphal <tinyscuba@hotmail.com>
2014-02-12 17:16:27 -0800

    write the content to an excel sheet

--- src/WorkSummarizer/Program.cs
+++ src/WorkSummarizer/Program.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using Events;
 using Events.Outlook;
 using Events.TeamFoundationServer;
 using Events.Yammer;
+using Microsoft.Office.Interop.Excel;
 
 namespace WorkSummarizer
 {
@@ -14,20 +16,25 @@ namespace WorkSummarizer
         {
             var plugins = new List<Type>();
 
-            // plugins.Add(typeof(OutlookPlugin));
+            plugins.Add(typeof(OutlookPlugin));
             // plugins.Add(typeof (TeamFoundationServerPlugin));
-             plugins.Add(typeof (YammerPlugin));
+            // plugins.Add(typeof (YammerPlugin));
 
             var pluginRuntime = new PluginRuntime();
             pluginRuntime.Start(plugins);
 
+            Application application = new Application();
+            Workbook workbook = application.Workbooks.Add();
+            Worksheet sheet = workbook.ActiveSheet;
+            int writingRowNumber = 1;
             foreach (var eventQueryServiceRegistration in pluginRuntime.EventQueryServices)
             {
                 Console.WriteLine("Querying from event query service: " + eventQueryServiceRegistration.Key);
                 
                 foreach(var evt in eventQueryServiceRegistration.Value.PullEvents(new DateTime(2014, 1, 1), new DateTime(2014, 2, 14)))
                 {
-                    Console.WriteLine("{0} {1}: {2}...", evt.Date, evt.Subject, evt.Text.Substring(0, Math.Min(evt.Text.Length, 30)).Replace("\n", String.Empty));
+                    //Console.WriteLine("{0} {1}: {2}...", evt.Date, evt.Subject, evt.Text.Substring(0, Math.Min(evt.Text.Length, 30)).Replace("\n", String.Empty));
+                    WriteRow(sheet, evt, writingRowNumber++);
                 }
                 
                 Console.WriteLine();
@@ -38,8 +45,10 @@ namespace WorkSummarizer
                 Console.WriteLine("No event query services registered!");
             }
 
-            Console.ReadKey();
+            application.Visible = true;
+            application.UserControl = true;
 
+            Console.ReadKey();
 
             /* example: grab TFS workitems and build a graph
             var tfs = new TfsData();
@@ -48,5 +57,13 @@ namespace WorkSummarizer
             var graph = TfsHelper.BuildWorkItemGraph(workItems);
             */
         }
+
+        private static void WriteRow(Worksheet sheet, Event theEvent, int row)
+        {
+            sheet.Cells[row, 1] = theEvent.Date;
+            sheet.Cells[row, 2] = theEvent.Subject.Text;
+            sheet.Cells[row, 3] = theEvent.Text;
+        }
+
     }
 }
