62da6c96dbee4f8e5deb48472523ab2647443284
Edmond Kotowski <ekotowski@gmail.com>
2014-02-12 16:11:32 -0800

    Add email query service file

---
+++ src/Events.Outlook/OutlookEmailEventQueryService.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Common;
+using Graph;
+using OutlookDataSource;
+
+namespace Events.Outlook
+{
+    public class OutlookEmailEventQueryService : IEventQueryService
+    {
+        private readonly IOutlookDataProvider m_outlookDataSource;
+
+        public OutlookEmailEventQueryService()
+        {
+            m_outlookDataSource = new OutlookDataProvider();
+        }
+
+        public IEnumerable<Event> PullEvents(DateTime startDateTime, DateTime stopDateTime)
+        {
+            var meetings = m_outlookDataSource.GetEmails(startDateTime, stopDateTime);
+
+            return meetings.Select(x =>
+                new Event()
+                {
+                    Text = x.Body,
+                    Subject = new Subject() { Text = x.Subject },
+                    Date = x.StartUtc,
+                    Duration = x.Duration.TotalMinutes,
+                    Participants =
+                        x.Recipients.Select(y => new Participant(y)).ToGraph()
+                });
+        }
+    }
+}
--- src/WorkSummarizer/Program.cs
+++ src/WorkSummarizer/Program.cs
@@ -8,13 +8,6 @@ namespace WorkSummarizer
         [STAThread] // this is for the Forms authentication dialog for Yammer auth....
         static void Main(string[] args)
         {
-            var meetingQueryService = new OutlookMeetingEventQueryService();
-            var events = meetingQueryService.PullEvents(DateTime.Parse("01/01/2014"), DateTime.Parse("02/11/2014"));
-            Console.WriteLine(events);
-
-            var emailQueryService = new OutlookEmailEventQueryService();
-            var emailEvents = emailQueryService.PullEvents(DateTime.Parse("01/01/2014"), DateTime.Parse("02/11/2014"));
-            Console.WriteLine(emailEvents);
         }
     }
 }
