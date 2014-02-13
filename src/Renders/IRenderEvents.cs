b0e2f6452520cee33b4f73033a6f857067eb7ab9
Matt Westphal <tinyscuba@hotmail.com>
2014-02-13 10:48:55 -0800

    adding renders framework

---
+++ src/Renders/IRenderEvents.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Events;
+
+namespace Renders
+{
+    public interface IRenderEvents
+    {
+        void WriteOut(IEnumerable<Event> events);
+    }
+}
