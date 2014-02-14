ed525a67329a2774cc327932828d5447feacf4cc
Matt Westphal <tinyscuba@hotmail.com>
2014-02-13 18:15:00 -0800

    added quick changes to show the top 10 stuff in excel

--- src/WorkSummarizer/Renders.Excel/ExcelWriteEvents.cs
+++ src/WorkSummarizer/Renders.Excel/ExcelWriteEvents.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Security.Cryptography;
 using Events;
 using Microsoft.Office.Interop.Excel;
 
@@ -9,26 +12,86 @@ namespace Renders.Excel
     {
         private Application application;
         private Workbook workbook;
-
-        public ExcelWriteEvents()
-        {
-           
-        }
-
+        
         public void Render(string eventType, IEnumerable<Event> events, IDictionary<string, int> weightedTags, IDictionary<string, int> weightedPeople, IEnumerable<string> importantSentences)
         {
             Worksheet sheet = GetSheet(eventType); 
            
             int writingRowNumber = 1;
 
+            WriteTags(sheet, writingRowNumber++, weightedTags);
+
+            WritePeople(sheet, writingRowNumber++, weightedPeople);
+
+            WriteSentences(sheet, writingRowNumber++, importantSentences);
+
             foreach (Event evt in events)
             {
                 WriteRow(sheet, evt, writingRowNumber++);
             }
             MarkUpColumnWidths(sheet);
-
+            
             application.Visible = true;
             application.UserControl = true;
+        }
+
+        private void WriteSentences(Worksheet sheet, int row, IEnumerable<string> importantSentences)
+        {
+            sheet.Cells[row++, 1] = "Top 10 Sentences";
+            var font = sheet.Range["A" + row].Font;
+            font.Bold = true;
+
+            if (importantSentences != null)
+            {
+                var sentences = (from s in importantSentences select s).Take(10);
+                foreach (var sentence in sentences)
+                {
+                    sheet.Cells[row++, 1] = sentence;
+                }
+            }
+            row++;
+        }
+
+        private void WritePeople(Worksheet sheet, int row, IDictionary<string, int> weightedPeople)
+        {
+            sheet.Cells[row++, 1] = "Top 10 People";
+            var font = sheet.Range["A" + row].Font;
+            font.Bold = true;
+
+            if (weightedPeople != null)
+            {
+                var ss = (from abc in weightedPeople select abc).OrderByDescending(s => s.Value).Take(10);
+
+                foreach (var aRow in ss)
+                {
+                    sheet.Cells[row, 1] = aRow.Key;
+                    sheet.Cells[row++, 2] = aRow.Value;
+                }
+            }
+            row++;
+        }
+
+        private void WriteTags(Worksheet sheet, int row, IDictionary<string, int> weightedTags)
+        {
+            sheet.Cells[row, 1] = "Top key words";
+            var font = sheet.Range["A" + row].Font;
+            font.Bold = true;
+            row ++;
+
+            if (weightedTags != null)
+            {
+                if (weightedTags != null)
+                {
+                    var ss = (from abc in weightedTags select abc).OrderByDescending(s => s.Value).Take(10);
+
+                    foreach (var aRow in ss)
+                    {
+                        sheet.Cells[row, 1] = aRow.Key;
+                        sheet.Cells[row++, 2] = aRow.Value;
+                    }
+                }
+            }
+            row++;
         }
 
         private static void MarkUpColumnWidths(Worksheet sheet)
