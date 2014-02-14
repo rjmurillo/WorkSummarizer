47a410b450e118ae0e9b4987ef4e2b7e14e45e3e
Edmond Kotowski <ekotowski@gmail.com>
2014-02-14 04:18:53 -0800

    Added identity service and it is currently commented out

--- src/DataSources.Who/IdentityUtility.cs
+++ src/DataSources.Who/IdentityUtility.cs
@@ -2,15 +2,17 @@ using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Net;
 using System.Text.RegularExpressions;
 using Common;
+using RestSharp;
 
 namespace DataSources.Who
 {
     public static class IdentityUtility
     {
         private static IdentityCache m_cache = new IdentityCache();
-        private static IEnumerable<string> m_exclusions = new[] {"Rollup Engine"};
+        private static IEnumerable<string> m_exclusions = new[] { "Rollup Engine" };
 
         public static string Sanitize(string input)
         {
@@ -24,11 +26,37 @@ namespace DataSources.Who
                 var pattern = "\\([^()]*\\)";
                 var regex = new Regex(pattern, RegexOptions.Compiled);
                 input = regex.Replace(input, string.Empty);
-                
+
+//                var client = new RestClient(String.Format("http://qeid01"));
+//                client.DefaultParameters.Clear();
+//                var request = new RestRequest(String.Format("api/v1/identity/{0}?fields=alias,display_name,enabled", input), Method.GET);
+//                request.AddHeader("Content-type", "text/json");
+//                request.AddHeader("Cache-Control", "max-age=86400");
+//                
+//                var response = client.Execute<IdentityModel>(request);
+//
+//                if(response.StatusCode == HttpStatusCode.OK && response.Data != null)
+//                {
+//                    input = response.Data.Alias;
+//                }
+//                else
+//                {
+//                    request = new RestRequest(String.Format("api/v1/identity?property=display_name&term={0}&fields=alias,display_name,enabled", input + '*'), Method.GET);
+//                    request.AddHeader("Content-type", "text/json");
+//                    request.AddHeader("Cache-Control", "max-age=86400");
+//
+//                    var displayNameResponse = client.Execute<IdentityModelList>(request);
+//
+//                    if (displayNameResponse.StatusCode != HttpStatusCode.OK || displayNameResponse.Data == null || displayNameResponse.Data.FirstOrDefault() == null)
+//                    {
+//                        return input;
+//                    }
+//
+//                    input = displayNameResponse.Data.First().Alias;
+//                }
 
                 var tp = new TeamProvider();
                 input = tp.ResolveDisplayName(input);
-
             }
 
             return input;
